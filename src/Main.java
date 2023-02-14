1b27316d664716e2e9f9776ffa9af46734b892ea
Жора <zhorabologov@gmal.com>
2023-02-14 17:39:13 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 1
+        for (int i = 1; i < 10 ; i++) {
+            System.out.println(i);}
+
+
+        //Задание 2
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);}
+
+        //Задание 3
+        for (int i = 0; i < 17; i = i + 2) {
+            System.out.println(i);}
+        
+        //Задание 4
+        for (int i = 10; i >= -10 ; i--) {
+            System.out.println(i);}
+
+        // Задание 5
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i);}
+
+        // Задание 6
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);}
+
+        // Задание 7
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(i);}
+
+
+
+
     }
 }
+
