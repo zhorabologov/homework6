4090b55db40b9d1da38c21bbcc787422bf5a55f1
Жора <zhorabologov@gmal.com>
2023-02-14 18:33:19 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -28,6 +28,32 @@ public class Main {
         // Задание 7
         for (int i = 1; i <= 512; i = i * 2) {
             System.out.println(i);}
+        
+        // Задание 8
+        int salary = 29_000;
+        int cash = 0;
+        for (int i = 1; i <=12 ; i++) {
+        cash += salary;
+            System.out.println("Месяц " + i + " ,сумма накоплений равна " + cash + "рублей");}
+
+        //Задание 9
+        double percent = 0.12;
+        double cashWithPercent = 0;
+        for (int i = 1; i <=12 ; i++) {
+            cashWithPercent = (cashWithPercent + salary) * (1 + percent) / 12;
+            System.out.println("Месяц " + i + " ,сумма накоплений равна " + cashWithPercent + "рублей");}
+        
+        // Задание 10
+        for(int i = 0; i < 10; i++){
+            for(int k = 0; k < 10; k++){
+                System.out.printf("%4d", (i + 1) * (k + 1));
+                System.out.println();
+    }
+            
+        }
+
+
+
 
 
 
