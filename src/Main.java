c54136cd5d6af4b7ffd828365ae26ad32e801abb
Жора <zhorabologov@gmal.com>
2023-02-27 01:14:06 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -62,11 +62,11 @@ public class Main {
         }
 
         //Задание 1.2
-        int i = 10;
-        while (i >= 1) {
-            System.out.print(i);
-            i--;
-            System.out.println(i);
+        int j = 10;
+        while (j >= 1) {
+            System.out.print(j);
+            j--;
+            System.out.println(j);
         }
 
         int k = 1;
@@ -79,24 +79,84 @@ public class Main {
         //Задание 1.3
 
         int population = 12000000;
+        int yearCount = 10;
         int born = 17;
         int death = 8;
-        int dif = born - death;
-        for (int j = 1; j <= 10; j++) {
-            population += population * dif / 1000;
+        for (int i = 1; i <= yearCount; ++i) {
+            population += population * yearCount / 1000;
         }
-        System.out.println("Численность населения составляет" + population);
+        System.out.println("Год" + yearCount + "Численность населения составляет" + population);
 
-        //Задание 1.4
+        //Задание 1.4 5
 
-        int sum = 12000000;
-        int initialAmount = 15000;
-        int p = 1;
-        while (i > 0) {
-            sum += initialAmount * 0.07;
+        double currentMoney = 15000;
+        double rate = 0.07;
+        int totalMoney = 12000000;
+        int currentMonth = 1;
+        while (currentMoney <= totalMoney) {
+            currentMoney *= (1 + rate);
+
+            if (currentMonth % 6 == 0) {
+                System.out.println("месяц" + currentMonth + " накопления " + currentMoney);
+            }
+            ++currentMonth;
         }
+        System.out.println("месяц" + currentMonth + " накопления " + currentMoney);
+
+        //Задание 1.6
+
+        double money = 15000;
+        double rateMonth = 0.07;
+
+        for (int month = 1; month < 9 * 12; ++month) {
+            money *= (1 + rateMonth);
+            if (month % 6 == 0) {
+                System.out.println("месяц" + month + " накопления " + money);
+            }
+        }
+
+        //Задание 1.7
+
+        int friday = 5;
+        while (friday <= 31) {
+            System.out.println("");
+            friday += 7;
+        }
+
+        for ( ; friday <= 31; friday += 7) {
+            System.out.println("");
+        }
+
+        for (int i = friday; i <= 31; i += 7) {
+            System.out.println(" Сегодня пятница, " + i + " число." + "Необходимо подготовить отчет");
+        }
+
+        //Задание 8
+
+        int carrentYear = 2023;
+        int fromYear = 200;
+        int toYear = carrentYear +100;
+
+        for (int i = fromYear; i <= toYear ; i++) {
+            if (i % 79 == 0) {
+                System.out.println(i);
+            }
+        }
+
+        for (int i = 79; i <= toYear; i += 79) {
+            if (i >= fromYear) {
+                System.out.println(i);
+            }
+        }
+
+
+
+        }
+
     }
-}
+
+
+
 
 
 
